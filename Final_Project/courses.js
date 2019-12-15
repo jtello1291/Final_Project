f2647a7a61c24b2d60bebc7e7b34ac2cd972f7ac
jtello1291 <55221603+jtello1291@users.noreply.github.com>
2019-12-14 23:20:20 -0500

    Add files via upload

---
+++ Final_Project/courses.js
@@ -0,0 +1,22 @@
+{
+  "CourseDetails":{
+    "courses":[
+      {
+        "title": "software application",
+        "code": "CUS 1102",
+        "instructor": "Lander Lugo, Glenda",
+        "day": "TF, 1700-1825",
+        "seats": "12",
+        "type": "Lecture"
+      },
+      {
+        "title": "software application",
+        "code": "CUS 1102",
+        "instructor": "Felicetti, Robert Alvaro",
+        "day": "TF, 1215-1340",
+        "seats": "5",
+        "type": "Lecture"
+      }
+    ]
+  }
+}
